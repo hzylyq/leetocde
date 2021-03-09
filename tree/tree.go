8a7df0b17c1ab8c2a128f75255c7d972e4f07b7b
胡志扬 <164177565@qq.com>
2021-03-09 21:07:24 +0800

    fix: conflict

--- tree/tree.go
+++ tree/tree.go
@@ -457,3 +457,99 @@ func (n *ChildNode) DepthFirstSearch(array []string) []string {
 	}
 	return nil
 }
+
+// Do not edit the class below except for
+// the insert, contains, and remove methods.
+// Feel free to add new properties and methods
+// to the class.
+func (tree *BST) Insert(value int) *BST {
+	if tree.Contains(value) {
+		return tree
+	}
+
+	if value < tree.Value {
+		if tree.Left == nil {
+			tree.Left = &BST{Value: value}
+		} else {
+			tree.Left.Insert(value)
+		}
+	} else {
+		if tree.Right == nil {
+			tree.Right = &BST{Value: value}
+		} else {
+			tree.Right.Insert(value)
+		}
+	}
+	return tree
+}
+
+func (tree *BST) Contains(value int) bool {
+	if tree == nil {
+		return false
+	}
+
+	if tree.Value == value {
+		return true
+	}
+
+	if tree.Value > value {
+		return tree.Left.Contains(value)
+	} else {
+		return tree.Right.Contains(value)
+	}
+}
+
+func (tree *BST) Remove(value int) *BST {
+	if !tree.Contains(value) {
+		return tree
+	}
+
+	tree.remove(value, nil)
+	return tree
+}
+
+func (tree *BST) remove(val int, parent *BST) {
+	if tree.Value > val {
+		if tree.Left != nil {
+			tree.Left.remove(val, tree)
+		}
+	} else if tree.Value < val {
+		if tree.Right != nil {
+			tree.Right.remove(val, tree)
+		}
+	} else {
+		if tree.Left != nil && tree.Right != nil {
+			tree.Value = tree.Right.getMinValue()
+			tree.Right.remove(tree.Value, tree)
+		} else if parent == nil {
+			if tree.Left != nil {
+				tree.Value = tree.Left.Value
+				tree.Left = tree.Left.Left
+				tree.Right = tree.Left.Right
+			} else if tree.Right != nil {
+				tree.Value = tree.Right.Value
+				tree.Left = tree.Right.Left
+				tree.Right = tree.Right.Right
+			}
+		} else if parent.Left == tree {
+			if tree.Left != nil {
+				parent.Left = tree.Left
+			} else {
+				parent.Left = tree.Right
+			}
+		} else if parent.Right == tree {
+			if tree.Left != nil {
+				parent.Right = tree.Left
+			} else {
+				parent.Right = tree.Right
+			}
+		}
+	}
+}
+
+func (tree *BST) getMinValue() int {
+	if tree.Left == nil {
+		return tree.Value
+	}
+	return tree.Left.getMinValue()
+}
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGenerateTrees(t *testing.T) {
@@ -119,6 +120,29 @@ func TestKthLargest(t *testing.T) {
 	KthLargest(root, 1)
 }
 
-func TestBST_InOrderTraverse(t *testing.T) {
-	tree
+func NewBST(val int) *BST {
+	return &BST{Value:val}
 }
+
+func TestBST_Insert(t *testing.T) {
+	root := NewBST(10)
+	root.Left = NewBST(5)
+	root.Left.Left = NewBST(2)
+	root.Left.Left.Left = NewBST(1)
+	root.Left.Right = NewBST(5)
+	root.Right = NewBST(15)
+	root.Right.Left = NewBST(13)
+	root.Right.Left.Right = NewBST(14)
+	root.Right.Right = NewBST(22)
+
+	root.Insert(12)
+	require.True(t, root.Right.Left.Left.Value == 12)
+
+	root.Remove(10)
+	require.True(t, root.Contains(10) == false)
+	require.True(t, root.Value == 12)
+
+	require.True(t, root.Contains(15))
+}
+
+
