f662a4aeb0b4359985ab448b4f6797d0cb2ef173
huzhiyang <164177565@qq.com>
2021-03-30 13:33:48 +0800

    fix: 74

--- array/array.go
+++ array/array.go
@@ -1,5 +1,23 @@
 package array
 
+import "sort"
+
+// 74. 搜索二维矩阵
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+
+	// 二分查找
+	m := len(matrix)
+	n := len(matrix[0])
+
+	i := sort.Search(m*n, func(i int) bool {
+		return matrix[i/n][i%n] >= target
+	})
+	return i < m*n && matrix[i/n][i%n] == target
+}
+
 // 1480. Running Sum of 1d Array
 func runningSum(nums []int) []int {
 	res := make([]int, len(nums))
