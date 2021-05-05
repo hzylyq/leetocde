917a9bee4dc263cd0b2718db0a129c36c9c299f4
huzhiyang <164177565@qq.com>
2021-05-05 23:14:47 +0800

    chore: traverse

---
+++ algoExpert/array/SpiralTraverse/spiral_traverse.go
@@ -0,0 +1,17 @@
+package SpiralTraverse
+
+func SpiralTraverse(array [][]int) []int {
+	if len(array) == 0 {
+		return nil
+	}
+
+	res := make([]int, 0)
+
+	m, n := len(array), len(array[0])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+
+		}
+	}
+	return res
+}
