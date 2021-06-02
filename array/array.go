625065ecae98598e2fd7b4d25859fd39cf1b1014
huzhiyang <164177565@qq.com>
2021-06-02 19:21:46 +0800

    chore: array

--- array/array.go
+++ array/array.go
@@ -7,10 +7,10 @@ import (
 // 27. 移除元素
 func removeElement(nums []int, val int) int {
 	left, right := 0, len(nums)
- 	for left < right {
- 		if nums[left] == val {
- 			nums[left] = nums[right-1]
- 			right--
+	for left < right {
+		if nums[left] == val {
+			nums[left] = nums[right-1]
+			right--
 		} else {
 			left++
 		}
@@ -23,11 +23,11 @@ func searchMatrix(matrix [][]int, target int) bool {
 	if len(matrix) == 0 {
 		return false
 	}
-
+	
 	// 二分查找
 	m := len(matrix)
 	n := len(matrix[0])
-
+	
 	i := sort.Search(m*n, func(i int) bool {
 		return matrix[i/n][i%n] >= target
 	})
@@ -41,7 +41,7 @@ func removeDuplicates(nums []int) int {
 	if length <= 2 {
 		return length
 	}
-
+	
 	fast, slow := 2, 2
 	for fast < length {
 		if nums[slow-2] != nums[fast] {
@@ -66,7 +66,7 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 			sorted = append(sorted, nums1[p:]...)
 			break
 		}
-
+		
 		if nums1[p] < nums2[q] {
 			sorted = append(sorted, nums1[p])
 			p++
@@ -75,7 +75,7 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 			q++
 		}
 	}
-
+	
 	copy(nums1, sorted)
 }
 
@@ -87,6 +87,28 @@ func containsDuplicate(nums []int) bool {
 			return true
 		}
 		set[num] = true
+	}
+	return false
+}
+
+// 523. 连续的子数组和 前缀和加求余
+func checkSubarraySum(nums []int, k int) bool {
+	if len(nums) < 2 {
+		return false
+	}
+	
+	mp := map[int]int{0: -1}
+	remainer := 0
+	
+	for i, num := range nums {
+		remainer = (remainer + num) % k
+		if prevIdx, ok := mp[remainer]; ok {
+			if i-prevIdx >= 2 {
+				return true
+			}
+		} else {
+			mp[remainer] = i
+		}
 	}
 	return false
 }
--- array/array_test.go
+++ array/array_test.go
@@ -1,7 +1,16 @@
 package array
 
-import "testing"
+import (
+	"testing"
+	
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMerge(t *testing.T) {
-	merge([]int{1,2,3,0,0,0}, 3, []int{2,5,6}, 3)
+	merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3)
+}
+
+func TestCheckSubarraySum(t *testing.T) {
+	res := checkSubarraySum([]int{0, 0}, 6)
+	assert.Equal(t, res, true)
 }
