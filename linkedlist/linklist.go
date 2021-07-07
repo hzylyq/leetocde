776a89423f25a0ae35b5632f3a3d6d5c001691c7
huzhiyang <164177565@qq.com>
2021-07-07 18:24:45 +0800

    fix: 回文链表

--- linkedlist/linklist.go
+++ linkedlist/linklist.go
@@ -121,3 +121,19 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	}
 	return pA
 }
+
+// 234. 回文链表
+func isPalindrome(head *ListNode) bool {
+	arr := make([]int, 0)
+
+	for ; head != nil; head = head.Next {
+		arr = append(arr, head.Val)
+	}
+
+	for i := 0; i < len(arr)/2; i++ {
+		if arr[i] != arr[len(arr)-i-1] {
+			return false
+		}
+	}
+	return true
+}
