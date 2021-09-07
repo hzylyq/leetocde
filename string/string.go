08fbba9c48d450c4e2ebaa7e13a3ca7c091ddbbb
huzhiyang <164177565@qq.com>
2021-09-07 18:47:38 +0800

    fix: string

--- string/string.go
+++ string/string.go
@@ -55,6 +55,28 @@ func checkRecord(s string) bool {
 	return true
 }
 
+// balancedStringSplit 1221. 分割平衡字符串
+// 在一个 平衡字符串 中，'L' 和 'R' 字符的数量是相同的。
+// 给你一个平衡字符串s，请你将它分割成尽可能多的平衡字符串。
+// 注意：分割得到的每个字符串都必须是平衡字符串。
+// 返回可以通过分割得到的平衡字符串的 最大数量 。
+func balancedStringSplit(s string) int {
+	var ans int
+	var d int
+	for _, char := range s {
+		if char == 'R' {
+			d++
+		} else {
+			d--
+		}
+
+		if d == 0 {
+			ans++
+		}
+	}
+	return ans
+}
+
 // 1576. 替换所有的问号
 func ModifyString(s string) string {
 	temp := make([]byte, len(s))
