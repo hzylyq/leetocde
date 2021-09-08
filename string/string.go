591019a37f6e8200d33ce4abf6ba8e6b1e65ec25
huzhiyang <164177565@qq.com>
2021-09-08 18:59:41 +0800

    fix: 最长字串

--- string/string.go
+++ string/string.go
@@ -1,5 +1,34 @@
 package string
 
+// 5. 最长回文子串
+// 给你一个字符串 s，找到 s 中最长的回文子串
+func longestPalindrome(s string) string {
+	if s == "" {
+		return ""
+	}
+
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		left1, right1 := expandAroundCenter(s, i, i)
+		left2, right2 := expandAroundCenter(s, i, i+1)
+
+		if right1-left1 > end-start {
+			start, end = left1, right1
+		}
+		if right2-left2 > end-start {
+			start, end = left2, right2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) (int, int) {
+	for ; left >= 0 && right < len(s) && s[left] == s[right]; left, right = left-1, right+1 {
+
+	}
+	return left + 1, right - 1
+}
+
 // 20. 有效的括号
 func isValid(s string) bool {
 	if len(s)%2 != 0 {
@@ -12,7 +41,7 @@ func isValid(s string) bool {
 		'{': '}',
 	}
 
-	stack := []rune{}
+	var stack []rune
 	for _, ch := range s {
 		if _, ok := pair[ch]; ok {
 			stack = append(stack, ch)
