a1d3fe84725b13f87c8fd1286c532e529284187c
huzhiyang <164177565@qq.com>
2021-10-11 14:38:30 +0800

    feat: lru

--- linkedlist/linklist.go
+++ linkedlist/linklist.go
@@ -1,5 +1,7 @@
 package linkedlist
 
+import "container/list"
+
 // This is an input struct. Do not edit.
 type ListNode struct {
 	Val  int
@@ -69,6 +71,65 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	}
 	second.Next = second.Next.Next
 	return dummy.Next
+}
+
+// LRUCache 146. LRU 缓存机制
+// 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
+// 实现 LRUCache 类：
+//
+// LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+// int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+// void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+//
+//
+// 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
+type LRUCache struct {
+	capacity int
+	cache    map[int]*list.Element
+	ll       *list.List
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		cache:    make(map[int]*list.Element, capacity),
+		capacity: capacity,
+		ll:       list.New(),
+	}
+}
+
+func (this *LRUCache) Get(key int) int {
+	if this.cache == nil {
+		return -1
+	}
+
+	if ee, ok := this.cache[key]; ok {
+		this.ll.MoveToFront(ee)
+		return ee.Value.(int)
+	}
+
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if this.cache == nil {
+		this.cache = make(map[int]*list.Element, this.capacity)
+	}
+
+	if ee, ok := this.cache[key]; ok {
+		this.ll.MoveToFront(ee)
+		ee.Value = value
+		return
+	}
+
+	ee := this.ll.PushFront(value)
+	this.cache[key] = ee
+	if this.capacity != 0 && this.ll.Len()>this.capacity {
+		ee := this.ll.Back()
+		if ee != nil {
+			this.ll.Remove(ee)
+			
+		}
+	}
 }
 
 // 206. 反转链表
