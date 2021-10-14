f2c414ce7318d834f32e92855b1e4e6c650d4bbb
huzhiyang <164177565@qq.com>
2021-10-14 10:54:35 +0800

    fix: 山脉数组

--- array/array.go
+++ array/array.go
@@ -382,6 +382,19 @@ func countPairs(deliciousness []int) int {
 	return res % (1e9 + 7)
 }
 
+// 剑指 Offer II 069. 山峰数组的顶部
+// 符合下列属性的数组 arr 称为 山峰数组（山脉数组） ：
+// arr.length >= 3
+// 存在 i（0 < i < arr.length - 1）使得：
+// arr[0] < arr[1] < ... arr[i-1] < arr[i]
+// arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+// 给定由整数组成的山峰数组 arr ，返回任何满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] 的下标 i ，即山峰顶部。
+func peakIndexInMountainArray(arr []int) int {
+	return sort.Search(len(arr)-1, func(i int) bool {
+		return arr[i] > arr[i+1]
+	})
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a
