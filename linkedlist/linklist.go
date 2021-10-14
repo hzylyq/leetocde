8f94d03b7f62a378aaff19fc9db9c2af79ff7606
huzhiyang <164177565@qq.com>
2021-10-14 13:57:18 +0800

    fix: 链表环

--- linkedlist/linklist.go
+++ linkedlist/linklist.go
@@ -99,6 +99,48 @@ func hasCycle(head *ListNode) bool {
 	return false
 }
 
+// 快慢指针
+func hasCycle2(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow, fast := head, head.Next
+	for fast != slow {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return true
+}
+
+// 142. 环形链表 II
+// 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+// 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+// 说明：不允许修改给定的链表。
+// 进阶：你是否可以使用 O(1) 空间解决此题？
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
+
 // LRUCache 146. LRU 缓存机制
 // 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
 // 实现 LRUCache 类：
@@ -193,23 +235,6 @@ func (c *LRUCache) removeTail() *dLinkedList {
 	node := c.tail.prev
 	c.removeNode(node)
 	return node
-}
-
-// 快慢指针
-func hasCycle2(head *ListNode) bool {
-	if head == nil || head.Next == nil {
-		return false
-	}
-
-	slow, fast := head, head.Next
-	for fast != slow {
-		if fast == nil || fast.Next == nil {
-			return false
-		}
-		fast = fast.Next.Next
-		slow = slow.Next
-	}
-	return true
 }
 
 // 160. 相交链表
