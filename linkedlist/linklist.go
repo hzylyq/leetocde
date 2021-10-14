b3ddd76e7ec55f1f71ba04aecef3dd55cbb72bd6
huzhiyang <164177565@qq.com>
2021-10-14 17:31:29 +0800

    fix: 链表排序

--- linkedlist/linklist.go
+++ linkedlist/linklist.go
@@ -237,6 +237,60 @@ func (c *LRUCache) removeTail() *dLinkedList {
 	return node
 }
 
+// 148. 排序链表
+// 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+// 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+func sortList(head *ListNode) *ListNode {
+	return sort(head, nil)
+}
+
+func sort(head, tail *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	if head.Next == tail {
+		head.Next = nil
+		return head
+	}
+
+	slow, fast := head, head
+	for fast != tail {
+		slow = slow.Next
+		fast = fast.Next
+		if fast != tail {
+			fast = fast.Next
+		}
+	}
+
+	mid := slow
+	return mergeList(sort(head, mid), sort(mid, tail))
+}
+
+func mergeList(head1, head2 *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	temp, temp1, temp2 := dummyHead, head1, head2
+	for temp1 != nil && temp2 != nil {
+		if temp1.Val <= temp2.Val {
+			temp.Next = temp1
+			temp1 = temp1.Next
+		} else {
+			temp.Next = temp2
+			temp2 = temp2.Next
+		}
+
+		temp = temp.Next
+	}
+
+	if temp1 != nil {
+		temp.Next = temp1
+	} else if temp2 != nil {
+		temp.Next = temp2
+	}
+
+	return dummyHead.Next
+}
+
 // 160. 相交链表
 // 快慢指针
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
