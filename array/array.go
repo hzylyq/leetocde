c5bdad844a13c77de6e020e87a40f44f793ad068
hzy <164177565@qq.com>
2021-11-11 09:58:02 +0800

    fix: 495

--- array/array.go
+++ array/array.go
@@ -284,6 +284,26 @@ func findDisappearedNumbers2(nums []int) []int {
 	return res
 }
 
+// 495. 提莫攻击
+// 在《英雄联盟》的世界中，有一个叫 “提莫” 的英雄。他的攻击可以让敌方英雄艾希（编者注：寒冰射手）进入中毒状态。
+// 当提莫攻击艾希，艾希的中毒状态正好持续 duration 秒。
+// 正式地讲，提莫在t发起发起攻击意味着艾希在时间区间[t, t + duration - 1]（含 t 和 t + duration - 1）处于中毒状态。如果提莫在中毒影响结束前再次攻击，中毒状态计时器将会重置，在新的攻击之后，中毒影响将会在 duration 秒后结束。
+// 给你一个 非递减 的整数数组timeSeries，其中 timeSeries[i] 表示提莫在 timeSeries[i] 秒时对艾希发起攻击，以及一个表示中毒持续时间的整数 duration 。
+// 返回艾希处于中毒状态的 总 秒数
+func findPoisonedDuration(timeSeries []int, duration int) int {
+	var ans, expired int
+
+	for _, t := range timeSeries {
+		if t >= expired {
+			ans += duration
+		} else {
+			ans += t + duration - expired
+		}
+		expired = t + duration
+	}
+	return ans
+}
+
 // 523. 连续的子数组和 前缀和加求余
 func checkSubarraySum(nums []int, k int) bool {
 	if len(nums) < 2 {
