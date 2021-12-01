59bd0c5b63ab3649e84fdc9b9eae1548834559bd
hzy <164177565@qq.com>
2021-12-01 20:11:33 +0800

    fix: 最短路径和

--- dp/dp.go
+++ dp/dp.go
@@ -1,6 +1,8 @@
 package dp
 
-func GenerateParenthesis(n int) []string {
+// 22. 括号生成
+// 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+func generateParenthesis(n int) []string {
 	var res []string
 
 	return nil
@@ -21,6 +23,39 @@ func helpGenerateParenthesis(idx, n, sum, add int, str string, res []string) {
 	if idx == n*2 {
 
 	}
+}
+
+// 64. 最小路径和
+// 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+// 说明：每次只能向下或者向右移动一步。
+func minPathSum(grid [][]int) int {
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
+	}
+
+	m := len(grid)
+	n := len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	dp[0][0] = grid[0][0]
+
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for i := 1; i < n; i++ {
+		dp[0][i] = dp[0][i-1] + grid[0][i]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+
+	return dp[m-1][n-1]
 }
 
 func MinCostClimbingStairs(cost []int) int {
