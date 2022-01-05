5ed4e19f477a8ff34116ed6e4ae2f53b00c85c62
hzy <164177565@qq.com>
2022-01-05 15:53:52 +0800

    fix: 26

--- array/array.go
+++ array/array.go
@@ -94,6 +94,25 @@ func LetterCombinations(digits string) []string {
 	return combs
 }
 
+// 26. 删除有序数组中的重复项
+// 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+// 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+func removeDuplicates1(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+
+	slow := 1
+	for fast := 1; fast < n; fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
+
 // 27. 移除元素
 func removeElement(nums []int, val int) int {
 	left, right := 0, len(nums)
--- dp/dp.go
+++ dp/dp.go
@@ -148,3 +148,32 @@ func max(a, b int) int {
 
 	return b
 }
+
+// 221. 最大正方形
+// 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+func maximalSquare(matrix [][]byte) int {
+	dp := make([][]int, 0)
+	maxSide := 0
+
+	for _, row := range matrix {
+		res := make([]int, 0)
+		for _, col := range row {
+			val := int(col - '0')
+			if val == 1 {
+				maxSide = 1
+			}
+		}
+
+		dp = append(dp, res)
+	}
+
+	for i := 1; i < len(matrix); i++ {
+		for j := 1; j < len(matrix[0]); j++ {
+			if dp[i][j] == 1 {
+
+			}
+		}
+	}
+
+	return 0
+}
