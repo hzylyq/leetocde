3e6c7a4eeb1440abcd322a0bb058bb369da25f64
hzy <164177565@qq.com>
2022-01-06 11:42:48 +0800

    fix: 71

--- search/search.go
+++ search/search.go
@@ -47,3 +47,24 @@ func searchRange(nums []int, target int) []int {
 	rightMost := sort.SearchInts(nums, target+1) - 1
 	return []int{leftMost, rightMost}
 }
+
+// 35. 搜索插入位置
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+// 请必须使用时间复杂度为 O(log n) 的算法。
+func searchInsert(nums []int, target int) int {
+	ans := len(nums)
+
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := (right-left)>>1 + left
+		if target <= nums[mid] {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return ans
+}
