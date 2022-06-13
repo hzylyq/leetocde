b18db1278d410fb24a49ab4cf13805335328bc90
huzhiyang <164177565@qq.com>
2022-06-13 19:08:40 +0800

    fix: 63"

--- dp/dp.go
+++ dp/dp.go
@@ -57,7 +57,31 @@ func uniquePaths(m int, n int) int {
 
 // 63. 不同路径 II
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
-	return 0
+	if len(obstacleGrid) == 0 {
+		return 1
+	}
+
+	m := len(obstacleGrid)
+	n := len(obstacleGrid[0])
+
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	if obstacleGrid[0][0] == 1 {
+		dp[0][0] = 0
+	} else {
+		dp[0][0] = 1
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+
+	return dp[m-1][n-1]
 }
 
 // 64. 最小路径和
