583c4011f2300f4592d33f43a72fbb5310511d85
huzhiyang <164177565@qq.com>
2022-07-06 19:04:01 +0800

    fix: binary search

--- sort/sort.go
+++ sort/sort.go
@@ -140,6 +140,23 @@ func searchMatrix3(matrix [][]int, target int) bool {
 	return false
 }
 
+// 704. 二分查找
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (right-left)/2 + left
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid + 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return -1
+}
+
 func min(a, b int) int {
 	if a < b {
 		return a
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -11,3 +11,7 @@ func TestBubbleSort(t *testing.T) {
 func TestMaxArea(t *testing.T) {
 	MaxArea([][]int{})
 }
+
+func TestSearch(t *testing.T) {
+	search([]int{-1, 0, 3, 5, 9, 12}, 9)
+}
