049874b6454e212181d94124937e0d797d304284
huzhiyang <164177565@qq.com>
2022-07-11 11:51:06 +0800

    fix: 876

--- linkedlist/linklist.go
+++ linkedlist/linklist.go
@@ -335,3 +335,15 @@ func deleteNode(node *ListNode) {
 	node.Val = node.Next.Val
 	node.Next = node.Next.Next
 }
+
+// 876. 链表的中间结点
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
