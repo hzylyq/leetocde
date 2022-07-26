06b43ffafc0bb37434633ba68782f9846fb2f9a3
huzhiyang <164177565@qq.com>
2022-07-26 20:35:41 +0800

    fix: skip list

--- linkedlist/linklist.go
+++ linkedlist/linklist.go
@@ -368,3 +368,23 @@ func middleNode(head *ListNode) *ListNode {
 
 	return slow
 }
+
+// 1206. 设计跳表
+type Skiplist struct {
+}
+
+func ConstructorSkipList() Skiplist {
+
+}
+
+func (this *Skiplist) Search(target int) bool {
+
+}
+
+func (this *Skiplist) Add(num int) {
+
+}
+
+func (this *Skiplist) Erase(num int) bool {
+
+}
