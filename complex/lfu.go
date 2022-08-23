ab1f7d437be69e945dbf14142767c17a335e0220
huzhiyang <164177565@qq.com>
2022-08-23 19:36:13 +0800

    chore: lfu

--- complex/lfu.go
+++ complex/lfu.go
@@ -52,3 +52,39 @@ package complex
  * param_3 := obj.GetMaxKey();
  * param_4 := obj.GetMinKey();
  */
+
+type LRUCache struct {
+	size     int
+	capacity int
+	
+	cache      map[int]*DLinkNode
+	head, tail *DLinkNode
+}
+
+type DLinkNode struct {
+	key, value int
+	prev, next *DLinkNode
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		size:     0,
+		capacity: capacity,
+		cache:    make(map[int]*DLinkNode, capacity),
+		head:     nil,
+		tail:     nil,
+	}
+}
+
+func (this *LRUCache) Get(key int) int {
+	value, ok := this.cache[key]
+	if !ok {
+		return 0
+	}
+	
+	return value.value
+}
+
+func (this *LRUCache) Put(key int, value int) {
+
+}
