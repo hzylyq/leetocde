c7c7314a64f4592a15f8294c9383dd9408bcb2b3
huzhiyang <164177565@qq.com>
2022-09-01 13:46:41 +0800

    fix: rust

--- rust/target/debug/deps/rust-99d3b767c107b8db.d
+++
@@ -1,5 +0,0 @@
-/home/huzhiyang/awe/src/hzy/leetocde/rust/target/debug/deps/rust-99d3b767c107b8db: src/main.rs
-
-/home/huzhiyang/awe/src/hzy/leetocde/rust/target/debug/deps/rust-99d3b767c107b8db.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/huzhiyang/awe/src/hzy/leetocde/rust/target/debug/rust: /home/huzhiyang/awe/src/hzy/leetocde/rust/src/main.rs
