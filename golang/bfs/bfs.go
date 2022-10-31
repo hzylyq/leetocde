b88688b00bd639e6dc13165cb76228cacf03c043
huzhiyang <164177565@qq.com>
2022-10-31 17:00:56 +0800

    fix: dp

---
+++ golang/bfs/bfs.go
@@ -0,0 +1,11 @@
+package bfs
+
+// 733. 图像渲染
+func floodFill(image [][]int, sr int, sc int, color int) [][]int {
+	currColor := image[sr][sc]
+	if currColor == color {
+		return image
+	}
+
+	return image
+}
--- golang/search/search.go
+++ golang/search/search.go
@@ -68,3 +68,29 @@ func searchInsert(nums []int, target int) int {
 
 	return ans
 }
+
+// 74. 搜索二维矩阵
+func searchMatrix(matrix [][]int, target int) bool {
+	// 二分查找
+
+	var (
+		m = len(matrix)
+		n = len(matrix[0])
+	)
+
+	left := 0
+	right := m*n - 1
+	for left <= right {
+		mid := (right-left)/2 + left
+		x := matrix[mid/n][mid%n]
+		if x < target {
+			left = mid + 1
+		} else if x > target {
+			right = mid - 1
+		} else {
+			return true
+		}
+	}
+
+	return false
+}
