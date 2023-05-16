c51192255fe2b7ea402c310475ed1ef2f1107736
huzhiyang <164177565@qq.com>
2023-05-16 15:03:50 +0800

    fix: 383

--- golang/hashmap/hash_map.go
+++ golang/hashmap/hash_map.go
@@ -136,6 +136,26 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 	return false
 }
 
+// 383. 赎金信
+func canConstruct(ransomNote string, magazine string) bool {
+	if len(ransomNote) > len(magazine) {
+		return false
+	}
+
+	cnt := [26]int{}
+	for _, ch := range magazine {
+		cnt[ch-'a']++
+	}
+
+	for _, ch := range ransomNote {
+		cnt[ch-'a']--
+		if cnt[ch-'a'] < 0 {
+			return false
+		}
+	}
+	return true
+}
+
 // 1365. 有多少小于当前数字的数字
 // 暴力破解
 func SmallerNumbersThanCurrent(nums []int) []int {
---
+++ rust/src/map/src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+
+pub struct Solution;
+
+impl Solution {
+    pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+        return  false
+    }
+}
